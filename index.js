e9355c3dcffa1533a26603926e8a732ef0c2da1f
Daniel Young <Danyoung91@gmail.com>
2022-11-03 20:41:36 -0400

    commit tree 16b6407624379c355a1770f473c288652feb8571

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let writeCards = (names, expletive) => {
+  let messages = [];
+  names.forEach((name) => {
+    console.log(`Thank you, ${name}, for the wonderful ${expletive} gift!`);
+    messages.push(`Thank you, ${name}, for the wonderful ${expletive} gift!`);
+  });
+  return messages;
+};
+
+let countDown = (p) => {
+  for (let i = 0; i <= p; i++) {
+    console.log(p - i);
+  }
+};
+
+countDown(4);
